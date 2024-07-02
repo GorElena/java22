8f45a7a753ce71f71b803014f6b77daa2796b66d
GorLena <zhura1977@list.ru>
2024-07-02 14:58:03 +0300

    README.md

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int replenishment = 1100;
+        int bonus = 1;
+
+        int result = replenishment + bonus;
+        if (replenishment != 1100) {
+            bonus = replenishment / 100;
+        }
+
+        System.out.println("Сумма пополнения счёта:"+ (replenishment)+ " рублей");
+        System.out.println("Итоговый счёт :" + (replenishment + bonus) + " рублей");
+        System.out.println("Количество бонусных рублей:" + (bonus) + "рублей");
+
     }
 }
+
