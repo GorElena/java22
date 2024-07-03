fc897bec901ef0b5cfef6389ec930cdc8978412d
GorLena <zhura1977@list.ru>
2024-07-03 13:51:53 +0300

    README.md

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
         int initalBalance = 100;// исходный баланс
-        int replenishment=1500;// сумма пополнения
+        int replenishment = 1500;// сумма пополнения
         int bonus;
-            bonus=1;
-        if (replenishment > 1000){
+        bonus = 1;
+        if (replenishment > 1000) {
             bonus = replenishment / 100;
         } else {
-            bonus =0;
+            bonus = 0;
         }
-        int balance= initalBalance +replenishment + bonus;
-        System.out.println("Начислено бонусов:"+ bonus);
-        System.out.println("Итоговый счёт :" +balance +"рублей");
+        int balance = initalBalance + replenishment + bonus;
+        System.out.println("Начислено бонусов:" + bonus);
+        System.out.println("Итоговый счёт :" + balance + "рублей");
 
 
     }
